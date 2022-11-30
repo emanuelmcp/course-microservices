fe3bb7b597298911ffb382954998cf358dfc5d6d
emanuelmcp <emanuelmcp@gmail.com>
2022-11-30 10:30:05 +0100

    Implemented load balancer and gateway

--- productos/src/main/java/io/github/emanuelmcp/productos/controllers/ProductController.java
+++ productos/src/main/java/io/github/emanuelmcp/productos/controllers/ProductController.java
@@ -3,25 +3,32 @@ package io.github.emanuelmcp.productos.controllers;
 import io.github.emanuelmcp.productos.api.services.IProductService;
 import io.github.emanuelmcp.productos.database.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
     private final IProductService productService;
+    private final Environment environment;
 
     @Autowired
-    public ProductController(IProductService productService) {
+    public ProductController(IProductService productService, Environment environment) {
         this.productService = productService;
+        this.environment = environment;
     }
 
 
     @GetMapping("/list")
     public List<Product> listProducts(){
-        return productService.findAll();
+        return productService.findAll().stream().map(p -> {
+            p.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+            return p;
+        }).collect(Collectors.toList());
     }
 
     @GetMapping("/list/{id}")
--- productos/src/main/java/io/github/emanuelmcp/productos/database/models/Product.java
+++ productos/src/main/java/io/github/emanuelmcp/productos/database/models/Product.java
@@ -2,17 +2,28 @@ package io.github.emanuelmcp.productos.database.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Product implements Serializable {
+
+    public Product(long id, String name, Double price, Date createAt) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.createAt = createAt;
+    }
+
     private long id;
     private String name;
     private Double price;
     private Date createAt;
+    @Value("${server.port}")
+    private Integer port;
 }
